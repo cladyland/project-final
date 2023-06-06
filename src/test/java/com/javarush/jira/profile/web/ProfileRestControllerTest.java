3e5e33293760fb1024c5568dacc7a03c69ad077c
Vika Kovalenko <shmoe761@gmail.com>
2023-06-06 12:38:18 +0300

    added new test for ProfileRestController and created auxiliary methods (some methods moved from data class to test class)

--- src/test/java/com/javarush/jira/profile/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/web/ProfileRestControllerTest.java
@@ -1,7 +1,11 @@
 package com.javarush.jira.profile.web;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javarush.jira.AbstractControllerTest;
 import com.javarush.jira.common.error.IllegalRequestDataException;
+import com.javarush.jira.profile.ContactTo;
+import com.javarush.jira.profile.ProfileTo;
 import com.javarush.jira.profile.internal.ProfileMapper;
 import com.javarush.jira.ref.RefType;
 import com.javarush.jira.ref.RefUtil;
@@ -10,15 +14,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Objects;
+import java.util.Set;
 
 import static com.javarush.jira.login.internal.web.UserTestData.ADMIN_MAIL;
 import static com.javarush.jira.login.internal.web.UserTestData.USER_MAIL;
 import static com.javarush.jira.profile.web.ProfileRestController.REST_URL;
 import static com.javarush.jira.profile.web.ProfileToTestData.PROFILE;
+import static com.javarush.jira.profile.web.ProfileToTestData.PROFILE_ID;
 import static com.javarush.jira.profile.web.ProfileToTestData.PROFILE_MATCHER;
-import static com.javarush.jira.profile.web.ProfileToTestData.UPDATED_JSON;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -34,6 +43,7 @@ class ProfileRestControllerTest extends AbstractControllerTest {
     @BeforeEach
     void reInit() {
         refUtil.updateRef(RefType.MAIL_NOTIFICATION);
+        refUtil.updateRef(RefType.CONTACT);
     }
 
     @Test
@@ -44,15 +54,16 @@ class ProfileRestControllerTest extends AbstractControllerTest {
                 .andDo(print())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(PROFILE_MATCHER.contentJson(PROFILE));
-
     }
 
     @Test
     @WithUserDetails(value = USER_MAIL)
     void update() throws Exception {
-        refUtil.updateRef(RefType.CONTACT);
+        Set<String> newMailNotifications = Set.of("three_days_before_deadline", "one_day_before_deadline");
+        Set<ContactTo> newContact = Set.of(new ContactTo("linkedin", "userLinkedin"));
+        ProfileTo updated = createUpdated(newMailNotifications, newContact);
 
-        perform(prepareBuilderForUpdate())
+        perform(prepareBuilderForUpdate(updated))
                 .andDo(print())
                 .andExpect(status().isNoContent());
     }
@@ -60,17 +71,47 @@ class ProfileRestControllerTest extends AbstractControllerTest {
     @Test
     @WithUserDetails(value = ADMIN_MAIL)
     void updateThrowExceptionWhenDifferentProfileAndUserIds() throws Exception {
-        refUtil.updateRef(RefType.CONTACT);
-
-        perform(prepareBuilderForUpdate())
+        perform(prepareBuilderForUpdate(PROFILE))
                 .andDo(print())
                 .andExpect(result -> assertTrue(result.getResolvedException() instanceof IllegalRequestDataException))
+                .andExpect(result -> assertEquals("ProfileTo must has id=2", resultMessage(result)))
                 .andExpect(status().isUnprocessableEntity());
     }
 
-    private MockHttpServletRequestBuilder prepareBuilderForUpdate() {
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateThrowExceptionWhenContactNotExist() throws Exception {
+        ProfileTo update = createUpdated(PROFILE.getMailNotifications(),
+                Set.of(new ContactTo("not_exist", "not exist")));
+
+        perform(prepareBuilderForUpdate(update))
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof IllegalArgumentException))
+                .andExpect(result -> assertEquals("Value with key not_exist not found", resultMessage(result)))
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    private String resultMessage(MvcResult result) {
+        return Objects.requireNonNull(result.getResolvedException()).getMessage();
+    }
+
+    private ProfileTo createUpdated(Set<String> mailNotifications, Set<ContactTo> contacts) {
+        return new ProfileTo(PROFILE_ID, mailNotifications, contacts);
+    }
+
+    private MockHttpServletRequestBuilder prepareBuilderForUpdate(ProfileTo profile) {
+        String json = convertToJson(profile);
+
         return MockMvcRequestBuilders.put(REST_URL)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(UPDATED_JSON);
+                .content(json);
+    }
+
+    private String convertToJson(ProfileTo profile) {
+        var objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(profile);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
